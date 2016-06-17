01819f3cde27204c38482e4fe527ea35f3e56e6f
millz60 <Matt@matts-macbook-pro.local>
2016-06-16 21:44:55 -0500

    Initial Commit

---
+++ WordProcessor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WordProcessor
+//
+//  Created by Matt Milner on 6/16/16.
+//  Copyright © 2016 Matt Milner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WordProcessor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WordProcessor
+//
+//  Created by Matt Milner on 6/16/16.
+//  Copyright © 2016 Matt Milner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
